422ed890d5e8fdff0e278ba5b37c862e278defa2
Paola Biscaro <paolabiscaroper@gmail.com>
2025-07-03 19:31:59 -0300

    EXERCICIO 10 - Tabuada

---
+++ exercicios/10-tabuada.js
@@ -0,0 +1,9 @@
+/*Crie um programa que dado um número imprima a tabuada por um número pré deifinido*/
+
+const num = 5
+const multiplicador = 200
+
+for(let i = 0; i <= multiplicador; i++){
+    console.log(`${i} X ${num} = `, num * i)
+}
+
