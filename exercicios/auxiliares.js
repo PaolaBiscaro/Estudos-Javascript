0a68baafacd27ffdadc9212b0a57efb2b547e7f2
Paola Biscaro <paolabiscaroper@gmail.com>
2025-07-04 18:51:16 -0300

    EXERCICIOS - criação de um arquivo auxiliar para modularização

--- exercicios/auxiliares.js
+++ exercicios/auxiliares.js
@@ -1,2 +1,11 @@
 /*Esse arquivo contem algumas funções para serem reutilizadas no decorrer dos exercicios dessa pasta. */
 
+function prints(texto){
+    console.log(texto);
+}
+
+module.exports = {prints} //Prepara o objeto
+
+
+
+
