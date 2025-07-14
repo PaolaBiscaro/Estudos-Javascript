0876467013379329b34b8dd934266f7f9369c3cf
Paola Biscaro <paolabiscaroper@gmail.com>
2025-07-14 12:32:46 -0300

    EXERCICIO 18 - Outra resolução para o ano bissexto

--- exercicios/18-ano_bissexto.js
+++ exercicios/18-ano_bissexto.js
@@ -1,9 +1,18 @@
-let ano = 2005
+let ano = 2004
 
 if(ano % 4 === 0 && ano % 100 !== 0){
     console.log(`O ano ${ano} é bissexto`)
 
-}else if(ano % 100 ===0 && ano % 400 === 0){
+}else if(ano % 100 === 0 && ano % 400 === 0){
+    console.log(`O ano ${ano} é bissexto`)
+
+}else{
+    console.log(`O ano ${ano} não é bissexto`)
+}
+
+//Outra resolução
+
+if((ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 == 0)){
     console.log(`O ano ${ano} é bissexto`)
 
 }else{
