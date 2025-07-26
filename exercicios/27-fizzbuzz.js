dfb839f748b34859554b30debf81cb54c2bfd0cc
Paola Biscaro <paolabiscaroper@gmail.com>
2025-07-26 20:22:45 -0300

    EXERCICIO 27 - Outra resolução

--- exercicios/27-fizzbuzz.js
+++ exercicios/27-fizzbuzz.js
@@ -11,10 +11,30 @@ while (i <= 100) {
     } else if ((i % 5 === 0) && !(i % 3 === 0)) {
         console.log('Buzz - ', i)
 
-    } else if ((i % 5 === 0) && (i % 3 === 0)){
+    } else if ((i % 5 === 0) && (i % 3 === 0)) {
         console.log('FizzBuzz - ', i)
 
     }
 
     i++
+}
+
+
+//Outra resolção
+
+let num = 1;
+while (num <= 100) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (num % 3 === 0) {
+        console.log("Fizz");
+    }
+    else if (num % 5 === 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(num);
+    }
+    num++;
 }
